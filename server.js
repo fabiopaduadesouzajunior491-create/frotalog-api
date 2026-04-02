b3ebdc7d94ca519f05c3b04e3444cda7aeb614eb
Fabio Júnior <fabiopaduadesouza491@gmail.com>
2026-04-02 14:56:25 -0300

    rota romaneio mock

--- server.js
+++ server.js
@@ -27,6 +27,41 @@ app.get('/cargas', (req, res) => {
     }
   ]);
 });
+app.get('/cargas/:numcar/romaneio', (req, res) => {
+  const numcar = req.params.numcar;
+
+  res.json({
+    carga: numcar,
+    placa: "HNL8J25",
+    motorista: "VILMAR",
+    ajudante1: "WAGNER",
+    ajudante2: "RENE",
+    entregas: [
+      {
+        codcli: 101,
+        cliente: "DECIO COMERCIO",
+        bairro: "ZONA RURAL",
+        seq: 1,
+        pallets: 12,
+        itens: 30,
+        peso: 91.09,
+        volume: 0.122,
+        creditos: 0
+      },
+      {
+        codcli: 102,
+        cliente: "MINIMERCADO ORNELAS",
+        bairro: "PARQUE SAO JORGE",
+        seq: 2,
+        pallets: 5,
+        itens: 10,
+        peso: 30,
+        volume: 0.05,
+        creditos: 2
+      }
+    ]
+  });
+});
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
